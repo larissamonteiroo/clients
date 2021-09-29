abb3683464e5897c88bfa205234ec1862ce7c9f9
Larissa Monteiro <larissa.marques.1999@hotmail.com>
2021-09-29 20:23:38 -0300

    Tratamento de erros criado e metodo FindById

--- backend/src/main/java/com/larissa/clients/services/ClientService.java
+++ backend/src/main/java/com/larissa/clients/services/ClientService.java
@@ -1,13 +1,17 @@
 package com.larissa.clients.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.larissa.clients.dtos.ClientDTO;
 import com.larissa.clients.entities.Client;
 import com.larissa.clients.repositories.ClientRepository;
+import com.larissa.clients.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -15,9 +19,18 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 	
-	public Page<ClientDTO> findAllPaged (PageRequest pageRequest){
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest){
 		Page<Client> page = repository.findAll(pageRequest);
 		
 		return page.map(x -> new ClientDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+
+		return new ClientDTO(entity);
 	}
 }
