35cea23d6f920755b6a26736812a8a4cc874926c
Larissa Monteiro <larissa.marques.1999@hotmail.com>
2021-09-29 19:54:38 -0300

    ClientDTO criado

--- backend/src/main/java/com/larissa/clients/services/ClientService.java
+++ backend/src/main/java/com/larissa/clients/services/ClientService.java
@@ -1,11 +1,23 @@
 package com.larissa.clients.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import com.larissa.clients.dtos.ClientDTO;
+import com.larissa.clients.entities.Client;
 import com.larissa.clients.repositories.ClientRepository;
 
 @Service
 public class ClientService {
 
+	@Autowired
+	private ClientRepository repository;
+	
+	public Page<ClientDTO> findAllPaged (PageRequest pageRequest){
+		Page<Client> page = repository.findAll(pageRequest);
+		
+		return page.map(x -> new ClientDTO(x));
+	}
 }
