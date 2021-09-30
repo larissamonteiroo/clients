e9836341cb8be40ea886c9b18e0b1477d7db2f1d
Larissa Monteiro <larissa.marques.1999@hotmail.com>
2021-09-29 21:01:50 -0300

    Tratamento de erros para o DELETE e metodo DELETE client

--- backend/src/main/java/com/larissa/clients/resources/ClientResource.java
+++ backend/src/main/java/com/larissa/clients/resources/ClientResource.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -62,5 +63,12 @@ public class ClientResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
 
 		return ResponseEntity.created(uri).body(dto);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		
+		return ResponseEntity.noContent().build();
 	}
 }
--- backend/src/main/java/com/larissa/clients/services/ClientService.java
+++ backend/src/main/java/com/larissa/clients/services/ClientService.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.larissa.clients.dtos.ClientDTO;
 import com.larissa.clients.entities.Client;
 import com.larissa.clients.repositories.ClientRepository;
+import com.larissa.clients.services.exceptions.DatabaseException;
 import com.larissa.clients.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -64,5 +67,17 @@ public class ClientService {
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found: " + id);
 		}
+	}
+
+
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found: " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}	
 	}
 }
